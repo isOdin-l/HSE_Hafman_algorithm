0a829f27cb467fd0c68e656b33653f97503f0b2d
Arbuz-ignor <maksimplov@gmail.com>
2025-02-19 19:11:10 +0300

    Cоздаем узел дерева

--- Создание дерева.cpp
+++ Создание дерева.cpp
@@ -2,3 +2,23 @@
 #include <vector>
 
 using namespace std;
+// Узел
+struct Node {
+    unsigned char ch;
+    int symb;
+    Node *left, *right;
+
+    // Конструктор
+    Node(unsigned char ch, int freq, Node *left = nullptr, Node *right = nullptr)
+            : ch(ch), symb(symb), left(left), right(right) {}
+};
+
+
+
+
+
+
+
+int main(){
+}
+
