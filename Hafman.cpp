df3b37bb6706d96d14da91c605e1572a5b4e8b31
Proshnick2016 <ivanbildagin@mail.ru>
2025-02-19 23:32:53 +0300

    add open file

--- Hafman.cpp
+++ Hafman.cpp
@@ -1,9 +1,10 @@
 ﻿#include <iostream>
 #include <vector>
+#include <fstream>
 #include <unordered_map>
 using namespace std;
 
-void compress(const vector<char>& data, const unordered_map<char, string>& huffman_code) {
+void compress(const vector<char>& data, const unordered_map<char, string>& huffman_code, const string& compressed_filename) {
     vector<bool> encoded_bits;
     for (char byte : data) {
         string code = huffman_code.at(byte);
@@ -11,6 +12,15 @@ void compress(const vector<char>& data, const unordered_map<char, string>& huffm
             encoded_bits.push_back(bit == '1');
         }
     }
+
+    ofstream outFile(compressed_filename, ios::binary);
+    if (!outFile) {
+        cout << "Cannot open file for writing!" << endl;
+        return;
+    }
+
+    int remainingBits = encoded_bits.size() % 8;
+    outFile.write(reinterpret_cast<const char*>(&remainingBits), sizeof(remainingBits));
 }
 
 int main()
