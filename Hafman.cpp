9424661282889c6b28928e15e9db7d7f3c1220dd
Proshnick2016 <ivanbildagin@mail.ru>
2025-02-19 23:04:48 +0300

    compress code

---
+++ Hafman.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+void compress(const vector<char>& data, const unordered_map<char, string>& huffman_code) {
+    vector<bool> encoded_bits;
+    for (char byte : data) {
+        string code = huffman_code.at(byte);
+        for (char bit : code) {
+            encoded_bits.push_back(bit == '1');
+        }
+    }
+}
+
+int main()
+{
+    cout << "Nadeusy eto zarabotaet" << endl;
+}
