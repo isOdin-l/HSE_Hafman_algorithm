5595ef2a4a5152659b94f308ae4fc0ccc087ee48
Nikita <syukhin.n@gmail.com>
2025-02-20 21:42:14 +0300

    Открытие файла для чтения сжатых данных

--- decompress.cpp
+++ decompress.cpp
@@ -5,3 +5,11 @@ include <iostream>
 using namespace std;
 
 int main(){}
+
+void decompressData(Node* root, const string& compressed_filename, const string& decompressed_filename) {
+    //открываем файл с сжатыми данными для чтения в бин.режиме
+    ifstream inFile(compressed_filename, ios::binary);
+    if (!inFile) {
+        cout << "Cannot open file for reading!" << endl;
+        return;
+    }
