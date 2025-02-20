fea6acdffa057a37b0275f456c58ad99dd9b23ed
Arbuz-ignor <maksimplov@gmail.com>
2025-02-20 17:15:58 +0300

    Добавляем компаратор, чтобы упорядочить кучу

--- Создание дерева.cpp
+++ Создание дерева.cpp
@@ -13,6 +13,12 @@ struct Node {
     Node(unsigned char ch, int symb, Node *left = nullptr, Node *right = nullptr)
             : ch(ch), symb(symb), left(left), right(right) {}
 };
+// Упорядачиваем кучу с помощью объекта сравнения
+struct comp {
+    bool operator()(Node* l, Node* r) {
+        return l->symb > r->symb;
+    }
+};
 
 // Это сохранение кодов в таблице вместе с обходом дерева
 void encode(Node* root, string str, unordered_map<unsigned char, string>& hf_Code) {
@@ -23,11 +29,13 @@ void encode(Node* root, string str, unordered_map<unsigned char, string>& hf_Cod
     encode(root->left, str + "0", hf_Code);
     encode(root->right, str + "1", hf_Code);
 }
+// 1. Создание дерева Хаффмана
 Node* build_Hf_Tree(const vector<unsigned char>& data, unordered_map<unsigned char, string>& build_Hf_Tree) {
     unordered_map<unsigned char, int> symb;
     for (unsigned char byte : data) {
         symb[byte]++;
     }
+
 }
 int main(){
 };
