ceae264d510e6b4f566f55c348be8e0fd94c458a
isOdin <ivanbildagin@mail.ru>
2025-02-20 19:42:27 +0300

    added write to file logic, added comments

--- Hafman.cpp
+++ Hafman.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 void compress(const vector<char>& data, const unordered_map<char, string>& huffman_code, const string& compressed_filename) {
     vector<bool> encoded_bits;
-    for (char byte : data) {
-        string code = huffman_code.at(byte);
-        for (char bit : code) {
+    for (char byte : data) { // проходим по всем байтам
+        string code = huffman_code.at(byte); // возвращает код Хафмана по текущему байту
+        for (char bit : code) { // перебираем каждый бит из Хафмана
             encoded_bits.push_back(bit == '1');
         }
     }
@@ -19,8 +19,20 @@ void compress(const vector<char>& data, const unordered_map<char, string>& huffm
         return;
     }
 
-    int remainingBits = encoded_bits.size() % 8;
-    outFile.write(reinterpret_cast<const char*>(&remainingBits), sizeof(remainingBits));
+    int remaining_bits = encoded_bits.size() % 8;
+    outFile.write(reinterpret_cast<const char*>(&remaining_bits), sizeof(remaining_bits)); // запись в начало файла первых (remaining_bits) битов
+
+    for (int i = 0; i < encoded_bits.size(); i += 8) { // перебираем биты по блокам = 8 битов
+        unsigned char byte = 0;
+        for (int j = 0; j < 8 && (i + j) < encoded_bits.size(); ++j) { // составляем из 8 битов целый байт
+            if (encoded_bits[i + j]) {
+                byte |= (1 << (7 - j));
+            }
+        }
+        outFile.write(reinterpret_cast<const char*>(&byte), sizeof(byte)); // запись полученного байта в файл
+    }
+    outFile.close();
+    cout << "Encoded data written to " << compressed_filename<< endl;
 }
 
 int main()
