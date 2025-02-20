7693e2ef03cf43658aec127fca2f6d781a66bc36
Nikita <syukhin.n@gmail.com>
2025-02-21 01:56:25 +0300

    Запись декодированных данных в файл

--- decompress.cpp
+++ decompress.cpp
@@ -41,14 +41,23 @@ void decompressData(Node* root, const string& compressed_filename, const string&
     }
     >>>>>>> decompress
     vector<unsigned char> decodedData;
-        Node* current = root;
-
-        //проходим по всем битам и восстанавливаем исходные данные
-        for (bool bit : decodedBits) {
-            //переходим по дереву Хаффмана в заивисимости от знач.бита
-            current = bit ? current->right : current->left;
-            if (!current->left && !current->right) {  //если достигли лист дерева, добавляем символ в декодекодированные данные
-                decodedData.push_back(current->ch);
-                current = root; //возвращение к корню дерева
-            }
+    Node* current = root;
+    
+    //проходим по всем битам и восстанавливаем исходные данные
+    for (bool bit : decodedBits) {
+        //переходим по дереву Хаффмана в заивисимости от знач.бита
+        current = bit ? current->right : current->left;
+        if (!current->left && !current->right) {  //если достигли лист дерева, добавляем символ в декодекодированные данные
+            decodedData.push_back(current->ch);
+            current = root; //возвращение к корню дерева
         }
+    }
+    ofstream outFile(decompressed_filename, ios::binary);
+        if (!outFile) {
+            cout << "Cannot open output file for writing!" << endl;
+            return;
+        }
+        outFile.write(reinterpret_cast<const char*>(decodedData.data()), decodedData.size());
+        outFile.close();
+        cout << "Decoded data written to " << decompressed_filename << endl;
+    }
