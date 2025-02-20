cb14d1639aaf3fb6d744d9c5c248c93622028978
Nikita <syukhin.n@gmail.com>
2025-02-20 22:49:35 +0300

    Чтение и декодирование битов из файла

--- decompress.cpp
+++ decompress.cpp
@@ -16,3 +16,12 @@ void decompressData(Node* root, const string& compressed_filename, const string&
     
     char* remaining_bits; //кол-во ост. битов
     inFile.read(reinterpret_cast<char*>(&remaining_bits), sizeof(remaining_bits));
+    
+    vector<bool> decodedBits;
+        char byte;
+        //читаем файл побайтово и преобразуем каждый байт в последовательность битов
+        while (inFile.read(&byte, sizeof(byte))) {
+            for (int i = 0; i < 8; ++i) {
+                decodedBits.push_back((byte >> (7 - i)) & 1); //каждый бит из байта добавили в массив декодированных битов
+            }
+        }
