17eb65a75b99c42cd9e7496af63e77aad0602b0c
isOdin <ivanbildagin@mail.ru>
2025-02-21 09:07:50 +0300

    changed filenames

--- main.cpp
+++ main.cpp
@@ -16,16 +16,17 @@ int main(){
     setlocale(LC_ALL, "ru_RU.UTF-8");
 
     vector<vector<string>> files = {
-        {"input", "txt"},  // Текстовый файл
-        {"input", "bmp"},  // Изображение
-        {"input", "wav"}   // Аудио
+        {"War_and_Peace", "txt"},  // Война и мир (на русском)
+        {"strange_castle", "bmp"},  // Изображение замка
+        {"Balck_square", "bmp"},  // Чёрный квадрат
+        {"matushka_zemlya", "wav"}   // песня матушка земля
     };
 
     for (int i = 0; i < files.size(); ++i) {
         string file = files[i][0] + "." + files[i][1];
         string compressedFile = files[i][0] + "_" + files[i][1] + "_comp.bin";
         string decompressedFile = files[i][0] + "_decomp." + files[i][1];
-        
+
         analyzeCompression(file, compressedFile, decompressedFile);
     }
 }
